9627f7056a20ff3cdbf9f262af97526d858eb219
bair0069 <bair0069@algonquinlive.com>
2022-03-31 13:23:32 -0400

    Add Person Model, People Router, Sanitize, Gift Model

--- models/Gift.js
+++ models/Gift.js
@@ -1,9 +1,16 @@
-/**
- * TODO:
- * import mongoose from "mongoose";
- * create schema for Gift
- */
+import mongoose from "mongoose";
+
 
 // has to include name, price, imageUrl, store, store.name, store.productUrl
+const schema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, maxlength: 64 , minlength: 4 },
+    price:{ type: Number, min: 100, default: 1000 },
+    imageUrl: { type: String, maxlength: 1024 },
+    store: { 
+      name: { type: String, maxlength: 254 }, 
+      productUrl: { type: String, maxlength: 1024 } 
+    },
+  })
 
-// export default mongoose.model("Gift", schema);
+export default mongoose.model("Gift", schema);
