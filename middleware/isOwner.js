e556fdc4a0cff33c79da69e2e68550391f135e8d
bair0069 <bair0069@algonquinlive.com>
2022-04-04 20:32:09 -0400

    isOwner checks if userId matches owner

--- middleware/isOwner.js
+++ middleware/isOwner.js
@@ -1,5 +1,29 @@
 //TODO:
 
-//import user from '../models/User.js';
+import Person from '../models/Person.js';
+import User from '../models/User.js';
 
 //export default async function (req, res, next) { // define user check for ownership}
+
+export default async function (req, res, next) {
+  const user = await User.findById(req.user._id);
+  const userId  = user._id;
+  const person = await Person.findById(req.params.id);
+  const owner = person.owner;
+  console.log(userId)
+  console.log(owner)
+
+  if (userId.toString() == owner.toString()) {
+  next ();
+  } else {
+    res.status(403).send({
+      errors: [
+        {
+          status: 403,
+          title: "Forbidden",
+          detail: "You are not authorized to perform this action.",
+        },
+      ],
+    });
+  }
+}
