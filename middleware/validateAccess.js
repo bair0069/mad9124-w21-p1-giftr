3b1a75c293b1effe34b2d4e3b4e796342e7c9fc1
bair0069 <bair0069@algonquinlive.com>
2022-04-10 19:34:48 -0400

    create valdiateAccess middleware

---
+++ middleware/validateAccess.js
@@ -0,0 +1,37 @@
+async function isOwner(personId, userId) {
+  const person = await Person.findById(personId);
+  const owner = person.owner;
+  if (userId === owner.toString()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+async function sharedWith(personId, userId) {
+  const person = await Person.findById(personId);
+  // console.log(person.sharedWith);
+  let isShared = false;
+  person.sharedWith.forEach((id) => {
+    if (id.toString() == userId) {
+      isShared = true;
+    }
+  });
+  return isShared;
+}
+
+export default function (res,req,next){
+  const person= await Person.findById(req.params.id);
+  const owner = person.owner;
+  if(userId === owner.toString()){
+    next();
+  }
+  else if (person.sharedWith.includes(userId)){
+    next();
+  }
+  else{
+    res.status(403).json({
+      error: "You do not have access to this resource"
+    });
+  }
+}
