89511ec5f32ca4bb88729596f6a3ca68e7353469
bair0069 <bair0069@algonquinlive.com>
2022-04-10 20:35:46 -0400

    Validate access

--- middleware/validateAccess.js
+++ middleware/validateAccess.js
@@ -1,39 +1,22 @@
+import Person from "../models/Person.js";
 
-
-async function isOwner(personId, userId) {
-  const person = await Person.findById(personId);
+export default async function (req, res, next) {
+  const userId = req.user._id;
+  const person = await Person.findById(req.params.id);
   const owner = person.owner;
   if (userId === owner.toString()) {
-    return true;
+    next();
+  } else if (person.sharedWith.includes(userId)) {
+    next();
   } else {
-    return false;
-  }
-}
-
-async function sharedWith(personId, userId) {
-  const person = await Person.findById(personId);
-  // console.log(person.sharedWith);
-  let isShared = false;
-  person.sharedWith.forEach((id) => {
-    if (id.toString() == userId) {
-      isShared = true;
-    }
-  });
-  return isShared;
-}
-
-export default function (res,req,next){
-  const person= await Person.findById(req.params.id);
-  const owner = person.owner;
-  if(userId === owner.toString()){
-    next();
-  }
-  else if (person.sharedWith.includes(userId)){
-    next();
-  }
-  else{
-    res.status(403).json({
-      error: "You do not have access to this resource"
+    res.status(403).send({
+      errors: [
+        {
+          status: 403,
+          title: "Forbidden",
+          detail: "You are not authorized to perform this action.",
+        },
+      ],
     });
   }
 }
