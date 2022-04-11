5dd8d4d7002dfc2065dc55f95681751d59d72f16
bair0069 <bair0069@algonquinlive.com>
2022-04-10 20:39:10 -0400

    create validate access

--- middleware/validateID.js
+++ middleware/validateID.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import Person from "../models/Person.js";
 import sendResourceNotFoundException from "../exceptions/ResourceNotFound.js";
 import log from "../startup/logger.js";
+import errorHandler from "./errorHandler.js";
 
 export default async function (req, res, next) {
   const personId = req.params.id;
@@ -10,36 +11,42 @@ export default async function (req, res, next) {
   try {
     if (mongoose.Types.ObjectId.isValid(personId)) {
       if (giftId) {
-        if (mongoose.Types.ObjectId.isValid(giftId)) {  
-          let gift = await Person.findOne({"gifts._id": giftId});
-          if (gift){
-            console.log("validateID: true");
-            next();
+        if (mongoose.Types.ObjectId.isValid(giftId)) {
+          let hasGift = await Person.findOne({ "gifts._id": giftId });
+          let person = await Person.findById(personId);
+          if (person) {
+            if (hasGift) {
+              console.log("validateID: true");
+              next();
+            } else {
+              console.log("validateID: false");
+              throw new sendResourceNotFoundException(
+                `Could not find a gift with id: ${giftId}`
+              );
+            }
+          } else {
+            throw new sendResourceNotFoundException(
+              `Could not find a person with id: ${personId}`
+            );
           }
-          else {
-            console.log("validateID: false");
-            throw new sendResourceNotFoundException(`Could not find a gift with id: ${giftId}`);
-          }
+        } else {
+          throw new sendResourceNotFoundException(
+            `Could not find a gift with id: ${giftId}`
+          );
         }
-        else{
-        throw new sendResourceNotFoundException(
-          `Could not find a gift with id: ${giftId}`
-        )
-      };
-      } else{
+      } else {
         if (await Person.findById(personId)) {
-        next();
+          next();
         }
         console.log("trying to find person");
       }
+    } else {
+      throw new sendResourceNotFoundException(
+        `Could not find a person with id: ${personId}`
+      );
     }
-    else{
-    throw new sendResourceNotFoundException(
-      `Could not find a person with id: ${personId}`
-    );
-  }
   } catch (err) {
     log.error(err);
-    next(err);
+    errorHandler(err, req, res);
   }
 }
