4196610dd415481ba10b0095951a886bb902b03b
bair0069 <bair0069@algonquinlive.com>
2022-04-11 00:23:55 -0400

    Clean up gifts and move formatResponseData to a middleware

--- routes/gifts.js
+++ routes/gifts.js
@@ -1,22 +1,28 @@
 import express from "express";
 import log from "../startup/logger.js";
-//formatResponseData formats the response data to be returned
+
+/**FORMAT RESPONSE DATA 
+ *formats the response data to be returned
+ *when called must be given a payload, and a type.
+*/
 import formatResponseData from "../helperFunctions/formatResponseData.js";
-//MODELS
+
+/**MODELS**
+ * Person is the model for a person
+ * Gift is the model for a gift
+ */
 import Gift from "../models/Gift.js";
 import Person from "../models/Person.js";
-//MIDDLEWARE
 
-//sanitize removes script tags from the request body
+/**MIDDLEWARE**
+sanitize removes script tags from the request body
+auth checks the users token
+validateId checks if the personId, or gift id is valid.
+validateAccess checks if the user is the owner of the resource or if the person is shared with the user
+*/
 import sanitize from "../middleware/sanitize.js";
-//auth checks the users token
 import auth from "../middleware/auth.js";
-
-//validateId checks if the personId, or gift id is valid.
 import validateId from "../middleware/validateID.js";
-
-//validateAccess checks if the user is the owner of the resource
-// or if the person is shared with the user
 
 import validateAccess from "../middleware/validateAccess.js";
 
@@ -38,7 +44,7 @@ router.post("/people/:id/gifts",auth,validateId,validateAccess,sanitize,async (r
       const person = await Person.findById(personId);
       await person.gifts.push(newGift);
       await person.save();
-      res.status(201).json(formatResponseData(newGift));
+      res.status(201).json(formatResponseData(newGift,'gifts'));
 
     } catch (err) {
       log.error(err);
@@ -56,7 +62,7 @@ router.patch("/people/:id/gifts/:giftId",auth,sanitize,validateId,validateAccess
           const gift = await person.gifts.id(giftId);
           gift.set(req.sanitizedBody);
           await person.save();
-          res.json(formatResponseData(gift));
+          res.json(formatResponseData(gift,'gifts'));
     } catch (err) {
       log.error(err);
       next(err);
@@ -70,7 +76,7 @@ router.delete("/people/:id/gifts/:giftId", auth,validateId,validateAccess, async
         const gift = await person.gifts.id(giftId);
         await person.gifts.id(giftId).remove();
         await person.save();
-        res.json(formatResponseData(gift));
+        res.json(formatResponseData(gift,'gifts'));
   } catch (err) {
     log.error(err);
     next(err);
