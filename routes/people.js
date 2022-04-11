3ce11aa6dff626c80116c3bdbee3beb3a1590314
bair0069 <bair0069@algonquinlive.com>
2022-04-11 01:44:27 -0400

    clean up people router

--- routes/people.js
+++ routes/people.js
@@ -1,11 +1,11 @@
 import express from "express";
 import Person from "../models/Person.js";
 import sanitize from "../middleware/sanitize.js";
-import mongoose from "mongoose";
 import auth from "../middleware/auth.js";
 import log from "../startup/logger.js";
-import ResourceNotFoundException from "../exceptions/ResourceNotFound.js";
 import validateId from "../middleware/validateID.js";
+import validateAccess from "../middleware/validateAccess.js";
+
 
 const router = express.Router();
 
@@ -15,31 +15,15 @@ router.get("/", auth, async (req, res) => {
     { owner: req.user._id },
     { sharedWith: req.user._id },
   ]);
-  res.status(201).json(people.map((person) => formatResponseData(person)));
+  res.status(201).json(people.map((person) => formatResponseData(person,'people')));
 });
 
 router.get("/:id", auth, validateId, async (req, res, next) => {
   const personId = req.params.id;
-  const userId = req.user._id;
   try {
-    if (
-      (await isOwner(personId, userId)) ||
-      (await sharedWith(personId, userId))
-    ) {
-      const person = await Person.findById(personId);
+      const person = await Person.findById(personId,'people');
       person.depopulate("gifts");
-      res.json(formatResponseData(person));
-    } else {
-      res.status(403).send({
-        errors: [
-          {
-            status: 403,
-            title: "Forbidden",
-            detail: "You are not authorized to perform this action.",
-          },
-        ],
-      });
-    }
+      res.json(formatResponseData(person,'people'));
   } catch (err) {
     log.error(err);
     next(err);
@@ -51,171 +35,53 @@ router.post("/", auth, sanitize, async (req, res, next) => {
   newPerson.owner = req.user._id;
   try {
     await newPerson.save();
-    res.status(201).json(formatResponseData(newPerson));
+    res.status(201).json(formatResponseData(newPerson,'people'));
   } catch (err) {
     log.error(err);
     next(err);
   }
 });
 
-router.patch("/:id", auth, sanitize, async (req, res, next) => {
+router.patch("/:id", auth, sanitize,validateId,validateAccess, async (req, res, next) => {
   const personId = req.params.id;
-  const userId = req.user._id;
   try {
-    if (await validateID(personId)) {
-      //check if ID is valid, if the check fails throw error
-      if (await isOwner(personId, userId)) {
         const object = await Person.findByIdAndUpdate(
           personId,
           req.sanitizedBody,
           { new: true, overwrite: false, runValidators: true }
         );
-        res.json(formatResponseData(object));
-      } else {
-        res.status(403).send({
-          errors: [
-            {
-              status: 403,
-              title: "Forbidden",
-              detail: "You are not authorized to perform this action.",
-            },
-          ],
-        });
-      }
-    } else {
-      throw new ResourceNotFoundException(
-        `Could not find a Person with id: ${personId}`
-      );
-    }
+        res.json(formatResponseData(object,'people'));
   } catch (err) {
     log.error(err);
     next(err);
   }
 });
 
-router.put("/:id", auth, sanitize, async (req, res, next) => {
+router.put("/:id", auth, sanitize,validateId,validateAccess, async (req, res, next) => {
   const personId = req.params.id;
   const userId = req.user._id;
   try {
-    if (await validateID(personId)) {
-      //check if ID is valid, if the check fails throw error
-      if (await isOwner(personId, userId)) {
         const object = await Person.findByIdAndUpdate(
           personId,
           { owner: userId, ...req.sanitizedBody },
           { new: true, overwrite: true, runValidators: true }
         );
-        res.json(formatResponseData(object));
-      } else {
-        res.status(403).send({
-          errors: [
-            {
-              status: 403,
-              title: "Forbidden",
-              detail: "You are not authorized to perform this action.",
-            },
-          ],
-        });
-      }
-    } else {
-      throw new ResourceNotFoundException(
-        `Could not find a Person with id: ${personId}`
-      );
-    }
+        res.json(formatResponseData(object,'people'));
   } catch (err) {
     log.error(err);
     next(err);
   }
 });
 
-router.delete("/:id", auth, async (req, res, next) => {
+router.delete("/:id", auth,validateId,validateAccess, async (req, res, next) => {
   const personId = req.params.id;
-  const userId = req.user._id;
   try {
-    if (await validateID(personId)) {
-      //check if ID is valid, if the check fails throw error
-      if (await isOwner(personId, userId)) {
         const person = await Person.findByIdAndRemove(personId);
-        res.json(formatResponseData(person));
-      } else {
-        res.status(403).send({
-          errors: [
-            {
-              status: 403,
-              title: "Forbidden",
-              detail: "You are not authorized to perform this action.",
-            },
-          ],
-        });
-      }
-    } else {
-      throw new ResourceNotFoundException(
-        `Could not find a Person with id: ${personId}`
-      );
-    }
+        res.json(formatResponseData(person,'people'));
   } catch (err) {
     log.error(err);
     next(err);
   }
 });
-
-//Helper functions
-/**
- * Format the response data object according to JSON:API v1.0
- * @param {string} type The resource collection name, e.g. 'cars'
- * @param {Object | Object[]} payload An array or instance object from that collection
- * @returns
- */
-
-function formatResponseData(payload, type = "people") {
-  if (payload instanceof Array) {
-    return { data: payload.map((resource) => format(resource)) };
-  } else {
-    return { data: format(payload) };
-  }
-
-  function format(resource) {
-    const { _id, ...attributes } = resource.toJSON
-      ? resource.toJSON()
-      : resource;
-    return { type, id: _id, attributes };
-  }
-}
-
-//check if the ID is valid and exists
-async function validateID(id) {
-  if (mongoose.Types.ObjectId.isValid(id)) {
-    if (await Person.findById(id)) {
-      return true;
-    }
-  }
-  return false;
-}
-
-//checks if the user is the owner of the Person
-async function isOwner(id, userId) {
-  const person = await Person.findById(id);
-  const owner = person.owner;
-
-  if (userId === owner.toString()) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-//check if shared with
-
-async function sharedWith(personId, userId) {
-  const person = await Person.findById(personId);
-  // console.log(person.sharedWith);
-  let isShared = false;
-  person.sharedWith.forEach((id) => {
-    if (id.toString() == userId) {
-      isShared = true;
-    }
-  });
-  return isShared;
-}
 
 export default router;
