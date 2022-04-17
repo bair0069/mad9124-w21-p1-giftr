1dcbd1a5cc4cb041fb7fe049aa44e6de438b6429
AKSHAY MAHAJAN <akshaymahajan@AKSHAYs-MacBook-Pro.local>
2022-04-17 15:25:17 -0400

    changed productUrl to productURL

--- models/Gift.js
+++ models/Gift.js
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
 
-
 // has to include name, price, imageUrl, store, store.name, store.productUrl
-const schema = new mongoose.Schema(
-  {
-    name: { type: String, required: true, maxlength: 64 , minlength: 4 },
-    price:{ type: Number, min: 100, default: 1000 },
-    imageUrl: { type: String, maxlength: 1024 },
-    store: { 
-      name: { type: String, maxlength: 254 }, 
-      productUrl: { type: String, maxlength: 1024 } 
-    },
-  })
+const schema = new mongoose.Schema({
+  name: { type: String, required: true, maxlength: 64, minlength: 4 },
+  price: { type: Number, min: 100, default: 1000 },
+  imageUrl: { type: String, maxlength: 1024 },
+  store: {
+    name: { type: String, maxlength: 254 },
+    productURL: { type: String, maxlength: 1024 },
+  },
+});
 
 export default mongoose.model("Gift", schema);
